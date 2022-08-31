fa51ec078c4a6d563e8dd2155aa417d46b70e018
Millan Bhandari <109762935+Millan-Bhandari@users.noreply.github.com>
2022-08-30 19:23:36 -0500

    commit

--- API JOKE TEST/ContentView.swift
+++ API JOKE TEST/ContentView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var setup = String()
+    @State private var delivery = String()
+    @State private var text = ""
     var body: some View {
-        Text("Hello, world!")
+        Text("Text")
+            .onTapGesture {
+                getJokes()
+            }
+            .onAppear {
+                getJokes()
+            }
+        Text(text)
             .padding()
+        Button("Button") {
+            getJokes()
+            text = setup
+        }
+    }
+    func getJokes() {
+        let apiKey = "?rapidapi-key=d57ad2586dmsh41318fe19aa4036p18c098jsn6cb0e627a4c9"
+        let query = "https://jokeapi-v2.p.rapidapi.com/joke/Any\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                    let contents = json.arrayValue
+                    for item in contents {
+                        let set = item["delivery"].stringValue
+                        setup.append(set)
+                }
+                return
+            }
+        }
     }
 }
 
