73d58dc8beddba3d87e11b0fd14ba96fc5790e8d
Millan Bhandari <109762935+Millan-Bhandari@users.noreply.github.com>
2022-08-30 19:04:33 -0500

    Initial Commit

---
+++ API JOKE TEST/API_JOKE_TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_JOKE_TESTApp.swift
+//  API JOKE TEST
+//
+//  Created by Millan Bhandari on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct API_JOKE_TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API JOKE TEST/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  API JOKE TEST
+//
+//  Created by Millan Bhandari on 8/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
